6275f8c16ce2253a51b05d80fd59664b69ad0a13
Fajar Fathurrahman <32007820+fajarfathurrahman@users.noreply.github.com>
2018-04-18 00:45:38 +0700

    Summary
    test new line

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <?php 
- echo 'Hello World \n'; 
- echo 'This is my simple php \n';
- echo 'commit remote from github \n';
+ echo "Hello World \r\n"; 
+ echo "This is my simple php \r\n";
+ echo "commit remote from github";
  ?> 
